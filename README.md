64eb2e161a10b10842863c2895218f25a41017b4
grakowski <grzegorz.rakowski@xtrf.eu>
2023-06-21 08:42:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GCPNoSpring
+
+### This is a non-Spring project, the execution of which once enters a new record into the Datastore in the "brep-playground" project.
+
+## Authentication
+To run the project, go to resources and add the new JSON file generated for the service account, then update the value in the FILE_PATH field in the Main class.
+The service account you are using must have permissions GCP brep-playground project.
---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,31 @@
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.datastore.Datastore
+import com.google.cloud.datastore.DatastoreOptions
+import com.google.cloud.datastore.Entity
+import com.google.cloud.datastore.Key
+import com.google.cloud.datastore.KeyFactory
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Main {
+
+    private static final String PROJECT_ID = "brep-playground"
+    private static final String FILE_PATH = "src/main/resources/brep-playground-key.json"
+    private static final String DATASTORE_KIND = "brep_manual_grakowki"
+
+    static void main(String[] args) {
+        DatastoreOptions datastoreOptions = DatastoreOptions.newBuilder()
+                .setProjectId(PROJECT_ID)
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(FILE_PATH)))
+                .build()
+
+        Datastore datastore = datastoreOptions.service
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind(DATASTORE_KIND)
+        Key key = keyFactory.newKey("keyName")
+        Entity entity = Entity.newBuilder(key)
+                .set("date_string", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")))
+                .build()
+        datastore.put(entity)
+    }
+}
